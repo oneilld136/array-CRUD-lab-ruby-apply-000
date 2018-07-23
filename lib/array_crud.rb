02755b188e0281bbaa5258828aee174b301dec63
oneilld136 <oneilld136@gmail.com>
2018-07-23 13:56:19 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  my_array=[]
 end
 
 def create_an_array
-  
+  harry_potter=["hermoine","ron","harry","dumbledore"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+  array << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array,element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array,index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
